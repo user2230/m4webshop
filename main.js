392256a558fc62e1797385410fe1e45f15f2eda4
user2230 <furkanefeceylan2008@hotmail.com>
2025-06-02 11:56:48 +0200

    last

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 let cartCount = 0;
 
 const products = [
-  ["spiederman.png", 59, "Spiederman - marvel", 4, true],
-  ["spiderman.png", 59, "Spiderman ps4 - Marvel", 5, false],
-  ["spiderps5.png", 59, "New ps5 spiderman", 5, true]
+  ["img/spiederman.png", 59, "Spiederman - marvel", "4 dagen", 4, true],
+  ["img/spiderman.png", 59, "Spiderman ps4 - Marvel", "2 min, 42 sec", 5, false],
+  ["img/spiderps5.png", 59, "New ps5 spiderman,", "30 min", 5, true]
 ];
+
+
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('div1');
   container.innerHTML = '';
 
   products.forEach(product => {
-    const [img, price, name, stars, inStock] = product;
+    const [img, price, name, scamtime, stars, inStock] = product;
     const card = document.createElement('div');
     card.className = 'card card-wrapper';
     card.innerHTML = `
       <img class="product-imgs" src="${img}" alt="${name}" width="100">
       <div class="product-price">$${price}</div>
       <div class="product-name">${name}</div>
+      <div class="product-scam">${scamtime}</div>
       <div class="stars">${'⭐'.repeat(stars)}</div>
       <button class="btn--working${!inStock ? ' out-of-stock' : ''}" ${!inStock ? 'disabled' : ''}>
         ${inStock ? 'Add to Cart' : 'Out of stock'}
@@ -25,6 +30,8 @@ window.addEventListener('DOMContentLoaded', () => {
     `;
     container.appendChild(card);
   });
+
+
 
   const cartCountElement = document.getElementById('cart-count');
   function updateCartCount() {
